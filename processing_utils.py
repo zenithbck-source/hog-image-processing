9844daf9e0a8f1cf4a8192defce0a5724a292e07
zenithbck-source <zenithbck@gmail.com>
2026-09-21 17:29:17 +0800

    Added extraction of HOG features from images

--- processing_utils.py
+++ processing_utils.py
@@ -1,5 +1,7 @@
 import os
 import cv2
+from skimage.feature import hog
+from skimage import exposure
 import matplotlib.pyplot as plt
 
 def sort_images():
@@ -38,13 +40,36 @@ def process_images(images):
 
     return processed_images
 
+def extract_image_hog(processed_image):
+    fd, hog_image = hog(processed_image, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=True)
+    hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
+    return fd, hog_image_rescaled
+
+def extracting_images(processed_images):
+    X = []
+    y = []
+    hog_images = []
+
+    for label, images_list in processed_images.items():
+        for image in images_list:
+            features, hog_image = extract_image_hog(image)
+            if features is not None:
+                X.append(features)
+                hog_images.append(hog_image)
+                if label == 'tennisball':
+                    y.append(0)
+                elif label == 'shuttlecock':
+                    y.append(1)
+
+    return X, y, hog_images
 
 
 if __name__ == "__main__":
     images = sort_images()
     processed_images = process_images(images)
+    X, y, hog_image = extracting_images(processed_images)
 
     plt.figure(figsize=(4, 4))
-    plt.imshow(processed_images['tennisball'][0], cmap='gray')
+    plt.imshow(hog_image[0], cmap='gray')
     plt.show()
 
